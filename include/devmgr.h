0799ac645fb4bd7bea2d2f10e49cdbab8f36d392
savent404 <savent_gate@outlook.com>
2020-11-08 21:29:10 -0800

    format devmgr interface

--- include/devmgr.h
+++ include/devmgr.h
@@ -1,17 +1,46 @@
 #pragma once
 
-#include <internal/type.h>
 #include <internal/toolchain.h>
+#include <internal/type.h>
 // #include <internal/driver.h>
 // #include <internal/device.h>
 
 __cbegin
 
+static inline device_id_t
+devmgr_register_device(const char* str)
+{
+    return 0;
+}
 
-static inline device_id_t devmgr_register_device(const char *str) { return 0; }
-static inline driver_id_t devmgr_register_driver(const char *str) { return 0; }
+static inline driver_id_t
+devmgr_register_driver(const char* str)
+{
+    return 0;
+}
 
-static inline const char * devmgr_query_device(device_id_t dev_id) { return "{}"; }
-static inline const char * devmgr_query_driver(driver_id_t dri_id) { return "{}"; }
+static inline const char*
+devmgr_query_device(device_id_t dev_id)
+{
+    return "{}";
+}
+
+static inline const char*
+devmgr_query_driver(driver_id_t dri_id)
+{
+    return "{}";
+}
+
+static inline int
+devmgr_update_device(device_id_t id, const char* str)
+{
+    return 0;
+}
+
+static inline int
+devmgr_update_driver(driver_id_t id, const char* str)
+{
+    return 0;
+}
 
 __cend
--- include/internal/devmgr.h
+++ include/internal/devmgr.h
@@ -1,37 +1,77 @@
 #pragma once
 
-#include <internal/type.h>
-#include <internal/toolchain.h>
-#include <internal/driver.h>
-#include <internal/device.h>
 #include <devmgr.h>
+#include <internal/device.h>
+#include <internal/driver.h>
+#include <internal/toolchain.h>
+#include <internal/type.h>
 
 __cbegin
 
+static inline const char*
+parse_devcie_to_json(device_t dev)
+{
+    return "{}";
+}
+static inline const char*
+parse_driver_to_json(driver_t dri)
+{
+    return "{}";
+}
 
-static inline device_id_t register_device(device_t dev) {
-    // TODO(savent): format dev to json string
-    const char *str = "{}";
+static inline device_t*
+parse_device_from_json(const char* p)
+{
+    return NULL;
+}
+static inline driver_t*
+parse_driver_from_json(const char* p)
+{
+    return NULL;
+}
+
+static inline device_id_t
+register_device(device_t dev)
+{
+    const char* str = parse_device_to_json(dev);
     return devmgr_register_device(str);
 }
-static inline driver_id_t register_driver(driver_t dri) {
-    // TODO(savent): format dri to json string
-    const char *str = "{}";
+
+static inline driver_id_t
+register_driver(driver_t dri)
+{
+    const char* str = parse_driver_to_json(dri);
     return devmgr_register_device(str);
 }
 
-static inline device_t query_device(device_id_t dev_id) {
-    const char *str = devmgr_query_device(dev_id);
-    // TODO(savent): parse json string to device_t
-    device_t dev = NULL;
+static inline device_t
+query_device(device_id_t dev_id)
+{
+    const char* str = devmgr_query_device(dev_id);
+    device_t dev = parse_device_from_json(str);
     return dev;
 }
 
-static inline driver_t query_driver(driver_id_t dri_id) {
-    const char *str = devmgr_query_driver(dri_id);
-    // TODO(savent): parse json string to driver_t
-    driver_t dri = NULL;
+static inline driver_t
+query_driver(driver_id_t dri_id)
+{
+    const char* str = devmgr_query_driver(dri_id);
+    driver_t dri = parse_driver_from_json(str);
     return dri;
+}
+
+static inline int
+update_device(device_t dev)
+{
+    const char* str = parse_device_to_json(dev);
+    return devmgr_update_device(dev_id(dev), str);
+}
+
+static inline int
+update_driver(driver_t dri)
+{
+    const char* str = parse_driver_to_json(dri);
+    return devmgr_update_driver(dri_id(dri), str);
 }
 
 __cend
