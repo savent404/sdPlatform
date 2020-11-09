be4dd18e94967657348dc2d68ace90b897c553b6
savent404 <savent_gate@outlook.com>
2020-11-08 21:29:49 -0800

    implement for '__init' things

--- include/internal/toolchain.h
+++ include/internal/toolchain.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stddef.h>
+
 // C/C++的C内容声明开头
 #define __cbegin
 
@@ -10,10 +12,14 @@
 #define EXTERN_C_API
 
 // 初始化程序段
-#define __init_code
+#define __init_code __attribute__((constructor)) __attribute__((section("init-code")))
 
 // 初始化数据段
-#define __init_data
+#define __init_data __attribute__((section("init-data")))
+
+// bytes align
+#define __align(n) __attribute__((aligned(n)))
+#define __aligned __align(sizeof(size_t))
 
 // 寄存器
 typedef void* reg_t;
