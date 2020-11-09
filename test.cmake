88d343e054e014af094a5550186b0b439f1f8bdb
savent404 <savent_gate@outlook.com>
2020-11-08 21:27:40 -0800

    rename test of dlist

--- test.cmake
+++ test.cmake
@@ -5,5 +5,5 @@ include_directories(include)
 
 enable_testing()
 
-add_executable(dlist-test tests/dlist.c)
+add_executable(dlist-test tests/dlist-test.c)
 ADD_TEST(NAME test_dlist_init COMMAND dlist-test)
