89f7291005eda4bf387eccb468bced4358a36b61
savent404 <savent_gate@outlook.com>
2020-11-09 01:03:05 -0800

    feature(mutex): add comments

--- include/internal/mutex.h
+++ include/internal/mutex.h
@@ -4,9 +4,45 @@
 
 typedef int mutex_t;
 
+/**
+ * alloc a new mutex instance
+ * @return mutex_t, NULL or 0 means failed
+ */
 static inline mutex_t mutex_new(void) { return 0; }
-static inline mutex_t mutex_del(mutex_t mutex) { return 0; }
-static inline int mutex_lock(mutex_t mutex) { return 0; }
-static inline int mutex_unlock(mutex_t mutex) { return 0; }
-static inline int mutex_trylock(mutex_t mutex) { return 0; }
+
+/**
+ * delete a mutex instance
+ * @note if mutex is locked, alse unlock & delete it.
+ * @param[in] mutex mutex_t
+ * @return true as success
+ */
+static inline bool mutex_del(mutex_t mutex) { return 0; }
+
+/**
+ * lock mutex
+ * @param[in] mutex mutex_t
+ * @return true as success
+ */
+static inline bool mutex_lock(mutex_t mutex) { return 0; }
+
+/**
+ * unlock mutex
+ * @param[in] mutex mutex_t
+ * @return true as success
+ */
+static inline bool mutex_unlock(mutex_t mutex) { return 0; }
+
+/**
+ * trylock mutex
+ * @note return quickly, no waiting for locked mutex
+ * @param[in] mutex mutex_t
+ * @return true as success
+ */
+static inline bool mutex_trylock(mutex_t mutex) { return 0; }
+
+/**
+ * checkout mutex is valide
+ * @param[in] mutex mutex_t
+ * @return true as valid
+ */
 static inline bool mutex_is_valid(mutex_t mutex) { return true; }
