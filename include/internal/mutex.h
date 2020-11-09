0855367ed371e96de3b7b9994b4d67a9ffe6a37f
savent404 <savent_gate@outlook.com>
2020-11-09 03:10:24 -0800

    implement mutex with pthread

--- include/internal/mutex.h
+++ include/internal/mutex.h
@@ -2,13 +2,21 @@
 
 #include <internal/type.h>
 
-typedef int mutex_t;
+#include <pthread.h>
+#include <stdlib.h>
+
+typedef pthread_mutex_t* mutex_t;
 
 /**
  * alloc a new mutex instance
- * @return mutex_t, NULL or 0 means failed
+ * @return mutex_t, use mutex_is_valid to check
  */
-static inline mutex_t mutex_new(void) { return 0; }
+static inline mutex_t mutex_new(void)
+{
+    mutex_t lock = (mutex_t)malloc(sizeof(*lock));
+    pthread_mutex_init(lock, NULL);
+    return lock;
+}
 
 /**
  * delete a mutex instance
@@ -16,21 +24,35 @@ static inline mutex_t mutex_new(void) { return 0; }
  * @param[in] mutex mutex_t
  * @return true as success
  */
-static inline bool mutex_del(mutex_t mutex) { return 0; }
+static inline bool mutex_del(mutex_t mutex)
+{
+    pthread_mutex_unlock(mutex);
+    int res = pthread_mutex_destroy(mutex);
+    if (!res) {
+        free(mutex);
+    }
+    return !res ? true : false;
+}
 
 /**
  * lock mutex
  * @param[in] mutex mutex_t
  * @return true as success
  */
-static inline bool mutex_lock(mutex_t mutex) { return 0; }
+static inline bool mutex_lock(mutex_t mutex)
+{
+    return pthread_mutex_lock(mutex) == 0;
+}
 
 /**
  * unlock mutex
  * @param[in] mutex mutex_t
  * @return true as success
  */
-static inline bool mutex_unlock(mutex_t mutex) { return 0; }
+static inline bool mutex_unlock(mutex_t mutex)
+{
+    return pthread_mutex_unlock(mutex) == 0;
+}
 
 /**
  * trylock mutex
@@ -38,11 +60,18 @@ static inline bool mutex_unlock(mutex_t mutex) { return 0; }
  * @param[in] mutex mutex_t
  * @return true as success
  */
-static inline bool mutex_trylock(mutex_t mutex) { return 0; }
+static inline bool mutex_trylock(mutex_t mutex)
+{
+    return pthread_mutex_trylock(mutex) == 0;
+}
 
 /**
  * checkout mutex is valide
  * @param[in] mutex mutex_t
  * @return true as valid
  */
-static inline bool mutex_is_valid(mutex_t mutex) { return true; }
+static inline bool mutex_is_valid(mutex_t mutex)
+{
+    return mutex == NULL;
+}
+
