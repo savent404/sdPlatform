1cf12ef36ca01739470500bdad699893b6f11c2b
savent404 <savent_gate@outlook.com>
2020-11-12 00:56:27 -0800

    add install rules

--- test.cmake
+++ test.cmake
@@ -6,7 +6,9 @@ include_directories(include)
 enable_testing()
 
 add_executable(dlist-test tests/dlist-test.c)
+install(TARGETS dlist-test RUNTIME DESTINATION bin/test)
 ADD_TEST(NAME test_dlist_init COMMAND dlist-test)
 
 add_executable(gpio-test tests/gpio-test.c)
 ADD_TEST(NAME gpio-test COMMAND gpio-test)
+install(TARGETS gpio-test RUNTIME DESTINATION bin/test)
