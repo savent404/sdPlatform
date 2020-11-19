f3f1cd2a15fc3f2ebf653e51e833d7a1c4a5710d
savent <savent_gate@outlook.com>
2020-11-19 12:39:29 +0800

    support for std::list and std::allocator

--- source/arm32-embedded-sys.cxx
+++ source/arm32-embedded-sys.cxx
@@ -17,6 +17,9 @@ __attribute__((weak)) void* operator new(size_t n) { return heap_alloc(n); }
 __attribute__((weak)) void operator delete(void* p) { heap_free(p); }
 __attribute__((weak)) void operator delete(void* p, size_t n) { heap_free(p); }
 
+__attribute__((weak)) void* operator new[](size_t n) { return heap_alloc(n); }
+__attribute__((weak)) void  operator delete[](void* p) { return heap_free(p); }
+
 #include <stdlib.h>
 
 extern "C" __attribute__((weak)) void* heap_alloc(size_t n) { return malloc(n); }
