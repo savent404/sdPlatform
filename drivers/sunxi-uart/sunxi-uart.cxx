5785e263d0f1f9ad162daa8a69f3e5b516858619
savent <savent_gate@outlook.com>
2020-12-01 14:37:09 +0800

    enhance bits usage

--- drivers/sunxi-uart/sunxi-uart.cxx
+++ drivers/sunxi-uart/sunxi-uart.cxx
@@ -12,6 +12,8 @@
 
 using namespace platform::drivers::uart;  // NOLINT
 
+using platform::bits;
+
 static int api_setup(runtime_ptr rt);
 static int api_tx(runtime_ptr rt, const char* buf, size_t len);
 static int api_config_parity(runtime_ptr rt);
@@ -44,24 +46,24 @@ driver_entry_level_default(sunxi_uart_entry);
 
 static void sunxi_uart_setup_clk(void* ccu_base, int uart_index) {
   /* reset uart */
-  static uint32_t* ccu_sw_reset_reg4 = platform::bits::io_addr_shift<uint32_t>(ccu_base, 0x2d8);
-  auto sw_reset4 = platform::bits::in(ccu_sw_reset_reg4);
-  sw_reset4 = platform::bits::clear_bit(sw_reset4, 16 + uart_index);
-  platform::bits::out(ccu_sw_reset_reg4, sw_reset4);
+  static uint32_t* ccu_sw_reset_reg4 = bits::shift_addr<uint32_t*>(ccu_base, 0x2d8);
+  auto sw_reset4 = bits::in(ccu_sw_reset_reg4);
+  sw_reset4 = bits::clear_bit(sw_reset4, 16 + uart_index);
+  bits::out(ccu_sw_reset_reg4, sw_reset4);
   // for (int i = 0; i < 100; i++) {
   // }
-  sw_reset4 = platform::bits::set_bit(sw_reset4, 16 + uart_index);
-  platform::bits::out(ccu_sw_reset_reg4, sw_reset4);
+  sw_reset4 = bits::set_bit(sw_reset4, 16 + uart_index);
+  bits::out(ccu_sw_reset_reg4, sw_reset4);
 
   /* enable gate */
-  static uint32_t* ccu_bcg_reg3 = platform::bits::io_addr_shift<uint32_t>(ccu_base, 0x6c);
-  auto bcg_reg3 = platform::bits::in(ccu_bcg_reg3);
-  bcg_reg3 = platform::bits::clear_bit(bcg_reg3, 16 + uart_index);
-  platform::bits::out(ccu_bcg_reg3, bcg_reg3);
+  static uint32_t* ccu_bcg_reg3 = bits::shift_addr<uint32_t*>(ccu_base, 0x6c);
+  auto bcg_reg3 = bits::in(ccu_bcg_reg3);
+  bcg_reg3 = bits::clear_bit(bcg_reg3, 16 + uart_index);
+  bits::out(ccu_bcg_reg3, bcg_reg3);
   // for (int i = 0; i < 100; i++) {
   // }
-  bcg_reg3 = platform::bits::set_bit(bcg_reg3, 16 + uart_index);
-  platform::bits::out(ccu_bcg_reg3, bcg_reg3);
+  bcg_reg3 = bits::set_bit(bcg_reg3, 16 + uart_index);
+  bits::out(ccu_bcg_reg3, bcg_reg3);
 }
 
 /**
@@ -75,27 +77,27 @@ static void sunxi_uart_setup_pinmux(int uart_index) {
   switch (uart_index) {
     case 2: {
       /* UART_2 tx */
-      auto tx_conf_reg = platform::bits::io_addr_shift<uint32_t>(pio_base, 0x128);
-      auto tx_pull_reg = platform::bits::io_addr_shift<uint32_t>(pio_base, 0x128);
-      auto tx_conf = platform::bits::in(tx_conf_reg);
-      tx_conf &= ~(0x7 << 8);
-      tx_conf |= 0x3 << 8;
-      platform::bits::out(tx_conf_reg, tx_conf);
-      auto tx_pull = platform::bits::in(tx_pull_reg);
+      auto tx_conf_reg = bits::shift_addr<uint32_t*>(pio_base, 0x128);
+      auto tx_pull_reg = bits::shift_addr<uint32_t*>(pio_base, 0x128);
+      auto tx_conf = bits::in(tx_conf_reg);
+      tx_conf = bits::clear_bit(tx_conf, 8, 3);
+      tx_conf = bits::or_bit(tx_conf, bits::shift_bits(0x03, 8));
+      bits::out(tx_conf_reg, tx_conf);
+      auto tx_pull = bits::in(tx_pull_reg);
       tx_pull &= ~(0x3 << 2 * 2);
       tx_pull |= 0x1 << 2 * 2;
-      platform::bits::out(tx_pull_reg, tx_pull);
+      bits::out(tx_pull_reg, tx_pull);
       /* UART_2 rx */
-      auto rx_conf_reg = platform::bits::io_addr_shift<uint32_t>(pio_base, 0x128);
-      auto rx_pull_reg = platform::bits::io_addr_shift<uint32_t>(pio_base, 0x128);
-      auto rx_conf = platform::bits::in(rx_conf_reg);
+      auto rx_conf_reg = bits::shift_addr<uint32_t*>(pio_base, 0x128);
+      auto rx_pull_reg = bits::shift_addr<uint32_t*>(pio_base, 0x128);
+      auto rx_conf = bits::in(rx_conf_reg);
       rx_conf &= ~(0x07 << 12);
       rx_conf |= 0x3 << 12;
-      platform::bits::out(rx_conf_reg, rx_conf);
-      auto rx_pull = platform::bits::in(rx_pull_reg);
+      bits::out(rx_conf_reg, rx_conf);
+      auto rx_pull = bits::in(rx_pull_reg);
       rx_pull &= ~(0x3 << 2 * 3);
       rx_pull |= 0x1 << 2 * 3;
-      platform::bits::out(rx_pull_reg, rx_pull);
+      bits::out(rx_pull_reg, rx_pull);
     } break;
     default:
       break;
@@ -113,8 +115,8 @@ int api_setup(runtime_ptr rt) {
   sunxi_uart_setup_pinmux(rt->uart_idx);
 
   /* enable fifo for rx/tx */
-  auto pfcr = platform::bits::io_addr_shift<int8_t>(rt->mem_base, SUNXI_UART_FCR);
-  platform::bits::out(pfcr, SUNXI_UART_FCR_RXTRG_1_2 | SUNXI_UART_FCR_TXTRG_1_2 | SUNXI_UART_FCR_FIFO_EN);
+  auto pfcr = bits::shift_addr<int8_t*>(rt->mem_base, SUNXI_UART_FCR);
+  bits::out(pfcr, SUNXI_UART_FCR_RXTRG_1_2 | SUNXI_UART_FCR_TXTRG_1_2 | SUNXI_UART_FCR_FIFO_EN);
 
   res = api_config_data_bit(rt);
   if (res) goto out;
@@ -130,13 +132,13 @@ int api_setup(runtime_ptr rt) {
 
   /* enable interrupts */
   {
-    auto ier_reg = platform::bits::io_addr_shift<uint32_t>(rt->mem_base, SUNXI_UART_IER);
-    platform::bits::out(ier_reg, 0x8d);
+    auto ier_reg = bits::shift_addr<uint32_t*>(rt->mem_base, SUNXI_UART_IER);
+    bits::out(ier_reg, 0x8d);
   }
   /* set MCR */
   {
-    auto mcr_reg = platform::bits::io_addr_shift<uint32_t>(rt->mem_base, SUNXI_UART_MCR);
-    platform::bits::out(mcr_reg, 0x03);
+    auto mcr_reg = bits::shift_addr<uint32_t*>(rt->mem_base, SUNXI_UART_MCR);
+    bits::out(mcr_reg, 0x03);
   }
 
 out:
@@ -144,87 +146,87 @@ out:
 }
 
 int api_config_parity(runtime_ptr rt) {
-  uint32_t* plcr = platform::bits::io_addr_shift<uint32_t>(rt->mem_base, SUNXI_UART_LCR);
+  uint32_t* plcr = bits::shift_addr<uint32_t*>(rt->mem_base, SUNXI_UART_LCR);
   uint32_t lcr = *plcr;
 
   // pairty
   switch (rt->parity) {
     case 0:
-      lcr = platform::bits::clear_bit(lcr, 3);
+      lcr = bits::clear_bit(lcr, 3);
       break;
     case 1:  // odd
-      lcr = platform::bits::set_bit(lcr, 3);
-      lcr = platform::bits::clear_bit(lcr, 4);
+      lcr = bits::set_bit(lcr, 3);
+      lcr = bits::clear_bit(lcr, 4);
       break;
     case 2:  // even
-      lcr = platform::bits::set_bit(lcr, 3);
-      lcr = platform::bits::clear_bit(lcr, 4);
+      lcr = bits::set_bit(lcr, 3);
+      lcr = bits::clear_bit(lcr, 4);
       break;
     default:  // as parameter invalid
       return eno::ENO_INVALID;
   }
-  platform::bits::out(plcr, lcr);
+  bits::out(plcr, lcr);
   return 0;
 }
 
 static int api_config_data_bit(runtime_ptr rt) {
-  uint32_t* plcr = platform::bits::io_addr_shift<uint32_t>(rt->mem_base, SUNXI_UART_LCR);
+  uint32_t* plcr = bits::shift_addr<uint32_t*>(rt->mem_base, SUNXI_UART_LCR);
   uint32_t lcr = *plcr;
   switch (rt->data_bits) {
     case 5:  // 5Bit - DLS(00)
-      lcr = platform::bits::clear_bit(lcr, 0);
-      lcr = platform::bits::clear_bit(lcr, 1);
+      lcr = bits::clear_bit(lcr, 0);
+      lcr = bits::clear_bit(lcr, 1);
       break;
     case 6:  // 6Bit - DLS(01)
-      lcr = platform::bits::set_bit(lcr, 0);
-      lcr = platform::bits::clear_bit(lcr, 1);
+      lcr = bits::set_bit(lcr, 0);
+      lcr = bits::clear_bit(lcr, 1);
       break;
     case 7:  // 7Bit - DLS(10)
-      lcr = platform::bits::clear_bit(lcr, 0);
-      lcr = platform::bits::set_bit(lcr, 1);
+      lcr = bits::clear_bit(lcr, 0);
+      lcr = bits::set_bit(lcr, 1);
       break;
     case 8:
-      lcr = platform::bits::set_bit(lcr, 0);
-      lcr = platform::bits::set_bit(lcr, 1);
+      lcr = bits::set_bit(lcr, 0);
+      lcr = bits::set_bit(lcr, 1);
       break;
     default:
       return eno::ENO_INVALID;
   }
-  platform::bits::out(plcr, lcr);
+  bits::out(plcr, lcr);
   return 0;
 }
 
 int api_config_stop_bit(runtime_ptr rt) {
-  uint32_t* plcr = platform::bits::io_addr_shift<uint32_t>(rt->mem_base, SUNXI_UART_LCR);
-  uint32_t lcr = platform::bits::in(plcr);
+  uint32_t* plcr = bits::shift_addr<uint32_t*>(rt->mem_base, SUNXI_UART_LCR);
+  uint32_t lcr = bits::in(plcr);
 
   switch (rt->stop_bits) {
     case 0:  // 1 bit
-      lcr = platform::bits::clear_bit(lcr, 2);
+      lcr = bits::clear_bit(lcr, 2);
       break;
     case 1:  // 1.5 bit
       // as sunxi-uart datasheet, 1.5 stop bit when DLS is zero
-      if (platform::bits::and_bit(lcr, (uint32_t)0x03) != 0) return eno::ENO_INVALID;
-      lcr = platform::bits::set_bit(lcr, 2);
+      if (bits::and_bit(lcr, (uint32_t)0x03) != 0) return eno::ENO_INVALID;
+      lcr = bits::set_bit(lcr, 2);
       break;
     case 2:
       // as sunxi-uart datasheet, 1.5 stop bit when DLS is zero
-      if (platform::bits::and_bit(lcr, (uint32_t)0x03) == 0) return eno::ENO_INVALID;
-      lcr = platform::bits::set_bit(lcr, 2);
+      if (bits::and_bit(lcr, (uint32_t)0x03) == 0) return eno::ENO_INVALID;
+      lcr = bits::set_bit(lcr, 2);
       break;
     default:
       return eno::ENO_INVALID;
   }
-  platform::bits::out(plcr, lcr);
+  bits::out(plcr, lcr);
   return 0;
 }
 
 int api_config_baud_rate(runtime_ptr rt) {
   // check ccn(rcc) find out APB2's freq
-  uint8_t* plcr = platform::bits::io_addr_shift<uint8_t>(rt->mem_base, SUNXI_UART_LCR);
-  uint8_t* pdll = platform::bits::io_addr_shift<uint8_t>(rt->mem_base, SUNXI_UART_DLL);
-  uint8_t* pdlh = platform::bits::io_addr_shift<uint8_t>(rt->mem_base, SUNXI_UART_DLH);
-  uint8_t* phalt = platform::bits::io_addr_shift<uint8_t>(rt->mem_base, SUNXI_UART_HALT);
+  uint8_t* plcr = bits::shift_addr<uint8_t*>(rt->mem_base, SUNXI_UART_LCR);
+  uint8_t* pdll = bits::shift_addr<uint8_t*>(rt->mem_base, SUNXI_UART_DLL);
+  uint8_t* pdlh = bits::shift_addr<uint8_t*>(rt->mem_base, SUNXI_UART_DLH);
+  uint8_t* phalt = bits::shift_addr<uint8_t*>(rt->mem_base, SUNXI_UART_HALT);
   size_t APB2_freq = 24 * 1000 * 1000;  // 24MHz
   size_t baud_rate = rt->baudrate;
   uint16_t dvi = 0;
@@ -232,38 +234,38 @@ int api_config_baud_rate(runtime_ptr rt) {
   dvi = static_cast<uint16_t>(res);
 
   /* hold tx so that uart will update lcr and baud in the gap of rx */
-  uint8_t halt = platform::bits::in(phalt);
-  halt = platform::bits::clear_bit(halt, 0);
-  halt = platform::bits::clear_bit(halt, 1);
-  platform::bits::out(phalt, halt | SUNXI_UART_HALT_FORCECFG | SUNXI_UART_HALT_HTX);
+  uint8_t halt = bits::in(phalt);
+  halt = bits::clear_bit(halt, 0);
+  halt = bits::clear_bit(halt, 1);
+  bits::out(phalt, halt | SUNXI_UART_HALT_FORCECFG | SUNXI_UART_HALT_HTX);
 
-  uint8_t lcr = platform::bits::in(plcr);
-  lcr = platform::bits::clear_bit(lcr, 7);
-  platform::bits::out(plcr, lcr | SUNXI_UART_LCR_DLAB);
-  platform::bits::out(pdll, dvi & 0xFF);
-  platform::bits::out(pdlh, dvi >> 8);
-  platform::bits::out(phalt, halt | SUNXI_UART_HALT_HTX | SUNXI_UART_HALT_FORCECFG | SUNXI_UART_HALT_LCRUP);
+  uint8_t lcr = bits::in(plcr);
+  lcr = bits::clear_bit(lcr, 7);
+  bits::out(plcr, lcr | SUNXI_UART_LCR_DLAB);
+  bits::out(pdll, dvi & 0xFF);
+  bits::out(pdlh, dvi >> 8);
+  bits::out(phalt, halt | SUNXI_UART_HALT_HTX | SUNXI_UART_HALT_FORCECFG | SUNXI_UART_HALT_LCRUP);
   do {
-    halt = platform::bits::in(phalt);
+    halt = bits::in(phalt);
   } while (halt & SUNXI_UART_HALT_LCRUP);
 
   /* cleanup halt */
-  platform::bits::out(plcr, lcr);
-  platform::bits::out(phalt, SUNXI_UART_HALT_FORCECFG);
+  bits::out(plcr, lcr);
+  bits::out(phalt, SUNXI_UART_HALT_FORCECFG);
   return 0;
 }
 
 int api_tx(runtime_ptr rt, const char* out, size_t len) {
-  uint32_t* thr = platform::bits::io_addr_shift<uint32_t>(rt->mem_base, SUNXI_UART_THR);
-  uint32_t* lsr = platform::bits::io_addr_shift<uint32_t>(rt->mem_base, SUNXI_UART_LSR);
+  uint32_t* thr = bits::shift_addr<uint32_t*>(rt->mem_base, SUNXI_UART_THR);
+  uint32_t* lsr = bits::shift_addr<uint32_t*>(rt->mem_base, SUNXI_UART_LSR);
   uint32_t x;
 
   while (len--) {
     do {
-      x = platform::bits::in(lsr);
-    } while (platform::bits::get_bit(*lsr, 6) == 0);
+      x = bits::in(lsr);
+    } while (bits::get_bit(*lsr, 6) == 0);
     x = *out++;
-    platform::bits::out(thr, x);
+    bits::out(thr, x);
   }
   return eno::ENO_OK;
 }
--- include/platform/bits.hxx
+++ include/platform/bits.hxx
@@ -15,50 +15,50 @@ namespace platform {
 
 struct bits {
   template <typename T>
-  static constexpr inline T in(const T* addr) {
+  static inline constexpr T in(const T* addr) {
     return *reinterpret_cast<volatile const T*>(addr);
   }
-  template <typename T1, typename T2=T1>
-  static constexpr inline void out(T1* addr, const T2 val) {
+  template <typename T1, typename T2 = T1>
+  static inline constexpr void out(T1* addr, const T2 val) {
     *reinterpret_cast<volatile T1*>(addr) = static_cast<const T2>(val);
   }
-  template <typename T>
-  static constexpr inline T* io_addr_shift(void* addr, const int offset) {
-    return (T*)( (int)addr + offset);
-    // return reinterpret_cast<T*>(reinterpret_cast<char*>(addr) + offset);
+  static inline constexpr int _shift_addr(const int addr, const int offset) { return addr + offset; }
+  template <typename _Tp_ret = void*, typename _Tp_param = void*>
+  static inline constexpr _Tp_ret shift_addr(_Tp_param addr, const int offset) {
+    return reinterpret_cast<_Tp_ret>(_shift_addr((int)addr, offset));
   }
   template <typename T>
-  static constexpr inline T* io_addr_shift(int addr, const int offset) {
-    return (T*)( (int)addr + offset);
-    // return reinterpret_cast<T*>(reinterpret_cast<char*>(addr) + offset);
+  static inline constexpr T shift_bits(const T val, const int offset) {
+    return val << offset;
   }
   template <typename T = int>
-  static constexpr T bit(const int offset) {
+  static inline constexpr T bit(const int offset) {
     return (1 << offset);
   }
-
-  template <typename T>
-  static constexpr T set_bit(const T val, const int bit_offset) {
-    return val | bit(bit_offset);
-  }
-
-  template <typename T>
-  static constexpr T clear_bit(const T val, const int bit_offset) {
-    return val & (~bit(bit_offset));
+  template <typename T = int>
+  static inline constexpr T bitmask(const int len, const int offset = 0) {
+    return ((1 << len) - 1) << offset;
   }
   template <typename T>
-  static constexpr int get_bit(const T val, const int bit_offset) {
-    return (val & bit<T>(bit_offset)) ? 1 : 0;
+  static inline constexpr T set_bit(const T val, const int bit_offset, const int len = 1) {
+    return val | bitmask<T>(len, bit_offset);
   }
   template <typename T>
-  static constexpr T and_bit(T v1, T v2) {
+  static inline constexpr T clear_bit(const T val, const int bit_offset, const int len = 1) {
+    return val & ~bitmask<T>(len, bit_offset);
+  }
+  template <typename T>
+  static inline constexpr int get_bit(const T val, const int bit_offset, const int len = 1) {
+    return (val & bitmask<T>(len, bit_offset)) >> bit_offset;
+  }
+  template <typename T1, typename T2>
+  static inline constexpr auto and_bit(const T1 v1, const T2 v2) {
     return v1 & v2;
   }
-  template <typename T>
-  static constexpr T or_bit(const T v1, const T v2) {
+  template <typename T1, typename T2>
+  static inline constexpr auto or_bit(const T1 v1, const T2 v2) {
     return v1 | v2;
   }
-
 };
 }  // namespace platform
 
