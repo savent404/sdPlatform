cff10478a296e5df7d9320ef1c0aca4b8f0351fd
savent <savent_gate@outlook.com>
2020-12-14 14:22:13 +0800

    封装 api-internal 缓存driver& device操作

--- source/api-internal.cxx
+++ source/api-internal.cxx
@@ -25,6 +25,7 @@
 #include <platform/msg.hxx>
 #include <platform/msg_wrapper.hxx>
 #include <platform/remotecall.hxx>
+#include <platform/debug.hxx>
 // clang-format on
 
 #ifndef __weak
@@ -40,124 +41,139 @@ using msg_ref = platform::msg_ref;
 extern "C" void heap_free(void *);
 /**
  * @brief 释放由 devmgr 返回的字符串内存
- * 
+ *
  * @param str 字符串指针
  */
 static void free_devmgr_string(const char *str) { heap_free(const_cast<char *>(str)); }
 
 /**
- * @brief 获取驱动的缓存集
+ * @brief 提供查询/修改dev/drv信息的方法
+ * @note 可缓存相关dev&drv信息用于减少调用devmgr的次数
  * 
- * @note 使用缓存的意义在于每次系统调用到来不需要通过devmgr api查询相关信息
- * @return std::map<int, platform::driver_dummy::driver_ptr> &
  */
-static auto &get_driver_map() {
-  static platform::alter::map<int, platform::driver_dummy::driver_ptr> *driver_map;
-  if (!driver_map) driver_map = new platform::alter::map<int, platform::driver_dummy::driver_ptr>;
-  return *driver_map;
-}
+struct internal_query {
+ public:
+  using debug = platform::debug;
+  using device_map_type = platform::alter::map<int, platform::device::device_ptr>;
+  using device_map_iter = device_map_type::iterator;
+  using driver_map_type = platform::alter::map<int, platform::driver_dummy::driver_ptr>;
+  using driver_map_iter = driver_map_type::iterator;
 
-/**
- * @brief 获取设备的缓存集
- * 
- * @note 使用缓存的意义在于每次系统调用到来不需要通过devmgr api查询相关信息
- * @return std::map<int, platform::device::device_ptr> &
- */
-static auto &get_device_map() {
-  static platform::alter::map<int, platform::device::device_ptr> *device_map;
-  if (!device_map) device_map = new platform::alter::map<int, platform::device::device_ptr>;
-  return *device_map;
-}
+ private:
+  driver_map_type driver_map;
+  device_map_type device_map;
 
-/**
- * @brief 查找合适的驱动
- * 
- * @note 优先从驱动缓存集中查询，查找不到则调用devmgr api
- * @param driver_id 驱动的id
- * @param item_iter 查找到的结果
- * @return int 0  成功
- *             !0 失败
- */
-static int search_driver(int driver_id, std::map<int, platform::driver_dummy::driver_ptr>::iterator *item_iter) {
-  auto iter = get_driver_map().find(driver_id);
-  if (iter == get_driver_map().end() || !(iter->second)) {
-    auto drv_str = devmgr_query_driver(driver_id);
-    if (!drv_str) {
-      return eno::ENO_NOTEXIST;
-    }
-    auto drv_ptr = new platform::driver_dummy;
-    if (!drv_ptr) {
-      return eno::ENO_NOMEM;
-    }
-    drv_ptr->from_json_str(drv_str);
-    free_devmgr_string(drv_str);
-    get_driver_map()[driver_id] = platform::driver_dummy::driver_ptr(drv_ptr);
-    iter = get_driver_map().find(driver_id);
+ public:
+  static internal_query &get_instance() {
+    static internal_query *instance;
+    if (!instance) instance = new internal_query;
+    debug::assert(instance != nullptr);
+    return *instance;
   }
-  *item_iter = iter;
-  return eno::ENO_OK;
-}
-
-/**
- * @brief 查找合适的设备
- * 
- * @note 优先从驱动缓存集中查询，查找不到则调用devmgr api
- * @param device_id 设备id
- * @param item_iter  查找到的结果
- * @return int 0  成功
- *             !0 失败
- */
-static int search_device(int device_id, std::map<int, platform::device::device_ptr>::iterator *item_iter) {
-  auto iter = get_device_map().find(device_id);
-  if (iter == get_device_map().end() || !(iter->second)) {
-    // need query device manager
-    auto dev_str = devmgr_query_device(device_id);
-    if (!dev_str) {
-      return eno::ENO_NOTEXIST;
+  /**
+   * @brief 获取驱动的缓存集
+   *
+   * @note 使用缓存的意义在于每次系统调用到来不需要通过devmgr api查询相关信息
+   * @return std::map<int, platform::driver_dummy::driver_ptr> &
+   */
+  static driver_map_type &get_driver_map() { return get_instance().driver_map; }
+  /**
+   * @brief 获取设备的缓存集
+   *
+   * @note 使用缓存的意义在于每次系统调用到来不需要通过devmgr api查询相关信息
+   * @return std::map<int, platform::device::device_ptr> &
+   */
+  static device_map_type &get_device_map() { return get_instance().device_map; }
+  /**
+   * @brief 查找合适的驱动
+   *
+   * @note 优先从驱动缓存集中查询，查找不到则调用devmgr api
+   * @param driver_id 驱动的id
+   * @param item_iter 查找到的结果
+   * @return int 0  成功
+   *             !0 失败
+   */
+  static int search_driver(int drv_id, driver_map_iter *item_iter) {
+    auto iter = get_driver_map().find(drv_id);
+    if (iter == get_driver_map().end() || !(iter->second)) {
+      // 未从缓存中找到该驱动
+      // 向devmgr查询该驱动信息
+      auto drv_str = devmgr_query_driver(drv_id);
+      if (!drv_str) {
+        return eno::ENO_NOTEXIST;
+      }
+      // 构造dummy driver并向其填充信息
+      auto drv_ptr = new platform::driver_dummy;
+      if (!drv_ptr) {
+        return eno::ENO_NOMEM;
+      }
+      drv_ptr->from_json_str(drv_str);
+      free_devmgr_string(drv_str);
+      get_driver_map()[drv_id] = platform::driver_dummy::driver_ptr(drv_ptr);
+      iter = get_driver_map().find(drv_id);
     }
-    auto dev_ptr = new platform::device();
-    if (!dev_ptr) {
-      return eno::ENO_NOMEM;
+    *item_iter = iter;
+    return eno::ENO_OK;
+  }
+  /**
+   * @brief 查找合适的设备
+   *
+   * @note 优先从驱动缓存集中查询，查找不到则调用devmgr api
+   * @param device_id 设备id
+   * @param item_iter  查找到的结果
+   * @return int 0  成功
+   *             !0 失败
+   */
+  static int search_device(int device_id, device_map_iter *item_iter) {
+    auto iter = get_device_map().find(device_id);
+    if (iter == get_device_map().end() || !(iter->second)) {
+      // need query device manager
+      auto dev_str = devmgr_query_device(device_id);
+      if (!dev_str) {
+        return eno::ENO_NOTEXIST;
+      }
+      auto dev_ptr = new platform::device();
+      if (!dev_ptr) {
+        return eno::ENO_NOMEM;
+      }
+      dev_ptr->from_json_str(dev_str);
+      free_devmgr_string(dev_str);
+      get_device_map()[device_id] = platform::device::device_ptr(dev_ptr);
+      iter = get_device_map().find(device_id);
     }
-    dev_ptr->from_json_str(dev_str);
-    free_devmgr_string(dev_str);
-    get_device_map()[device_id] = platform::device::device_ptr(dev_ptr);
-    iter = get_device_map().find(device_id);
+    *item_iter = iter;
+    return eno::ENO_OK;
   }
-  *item_iter = iter;
-  return eno::ENO_OK;
-}
-
-/**
- * @brief 通过devmgr更新device相关信息
- * 
- * @param device_id 设备id
- * @return int 0 成功
- */
-static int update_device_info(int device_id) {
-  const char *dev_str = devmgr_query_device(device_id);
-  if (!dev_str) return eno::ENO_NOTEXIST;
-  auto iter = get_device_map().end();
-  int res = search_device(device_id, &iter);
-  if (res) return res;
-  iter->second->from_json_str(dev_str);
-  return eno::ENO_OK;
-}
-
-/**
- * @brief 通过设备id获取该设备已绑定的驱动id
- * 
- * @param device_id 设备id
- * @return int id
- */
-static int get_driver_id(int device_id) {
-  auto iter = get_device_map().end();
-  int res = search_device(device_id, &iter);
-  if (res) {
-    return res;
+  /**
+   * @brief 通过devmgr更新device相关信息
+   *
+   * @param device_id 设备id
+   * @return int 0 成功
+   */
+  static int update_device_info(int device_id) {
+    const char *dev_str = devmgr_query_device(device_id);
+    if (!dev_str) return eno::ENO_NOTEXIST;
+    auto iter = get_device_map().end();
+    int res = search_device(device_id, &iter);
+    if (res) return res;
+    iter->second->from_json_str(dev_str);
+    return eno::ENO_OK;
   }
-  return iter->second->get_bind_id();
-}
+  /**
+   * @brief 通过设备id获取该设备已绑定的驱动id
+   *
+   * @param device_id 设备id
+   * @return int id
+   */
+  static int get_driver_id(int device_id) {
+    auto iter = get_device_map().end();
+    int res = search_device(device_id, &iter);
+    if (res) {
+      return res;
+    }
+    return iter->second->get_bind_id();
+  }
+};
 
 /**
  * @brief 生成函数的hash id
@@ -177,8 +193,8 @@ static auto gen_hash(const char *method, const char *driver_name) {
 extern "C" {
 
 int dev_bind(int device_id, int driver_id) {
-  auto iter = get_driver_map().end();
-  int res = search_driver(driver_id, &iter);
+  internal_query::driver_map_iter iter;
+  int res = internal_query::search_driver(driver_id, &iter);
   if (res) return res;
   auto hash = gen_hash("bind", iter->second->get_name());
   msg msgbuf = msg_wrapper::package(hash, device_id);
@@ -187,12 +203,12 @@ int dev_bind(int device_id, int driver_id) {
 }
 
 int dev_unbind(int device_id) {
-  int driver_id = get_driver_id(device_id);
+  int driver_id = internal_query::get_driver_id(device_id);
   if (!driver_id) return eno::ENO_NOTEXIST;
-  auto iter = get_driver_map().end();
-  int res = search_driver(driver_id, &iter);
+  internal_query::driver_map_iter iter;
+  int res = internal_query::search_driver(driver_id, &iter);
   if (res) return res;
-  update_device_info(device_id);
+  internal_query::update_device_info(device_id);
   auto hash = gen_hash("unbind", iter->second->get_name());
   auto msgbuf = msg_wrapper::package(hash, device_id);
   platform::rcall_client client(iter->second->get_ipc_remote_channel());
@@ -200,9 +216,9 @@ int dev_unbind(int device_id) {
 }
 
 int dev_open(int device_id, int flags) {
-  int driver_id = get_driver_id(device_id);
-  auto iter = get_driver_map().end();
-  int res = search_driver(driver_id, &iter);
+  int driver_id = internal_query::get_driver_id(device_id);
+  internal_query::driver_map_iter iter;
+  int res = internal_query::search_driver(driver_id, &iter);
   if (res) return res;
   auto hash = gen_hash("open", iter->second->get_name());
   auto msgbuf = msg_wrapper::package(hash, device_id, flags);
@@ -211,9 +227,9 @@ int dev_open(int device_id, int flags) {
 }
 
 int dev_close(int device_id) {
-  int driver_id = get_driver_id(device_id);
-  auto iter = get_driver_map().end();
-  int res = search_driver(driver_id, &iter);
+  int driver_id = internal_query::get_driver_id(device_id);
+  internal_query::driver_map_iter iter;
+  int res = internal_query::search_driver(driver_id, &iter);
   if (res) return res;
   auto hash = gen_hash("close", iter->second->get_name());
   auto msgbuf = msg_wrapper::package(hash, device_id, driver_id);
@@ -222,9 +238,9 @@ int dev_close(int device_id) {
 }
 
 int dev_transfer(int device_id, const void *in, size_t in_len, void *out, size_t out_len) {
-  int driver_id = get_driver_id(device_id);
-  auto iter = get_driver_map().end();
-  int res = search_driver(driver_id, &iter);
+  int driver_id = internal_query::get_driver_id(device_id);
+  internal_query::driver_map_iter iter;
+  int res = internal_query::search_driver(driver_id, &iter);
   if (res) return res;
   auto hash = gen_hash("transfer", iter->second->get_name());
   msg_ref refmsg(out, out_len);
@@ -234,9 +250,9 @@ int dev_transfer(int device_id, const void *in, size_t in_len, void *out, size_t
   return client._call(0, msgbuf.get(), msgbuf.size(), refmsg.get(), &sz);
 }
 int dev_write(int device_id, const void *in, size_t len) {
-  int driver_id = get_driver_id(device_id);
-  auto iter = get_driver_map().end();
-  int res = search_driver(driver_id, &iter);
+  int driver_id = internal_query::get_driver_id(device_id);
+  internal_query::driver_map_iter iter;
+  int res = internal_query::search_driver(driver_id, &iter);
   if (res) return res;
   auto hash = gen_hash("write", iter->second->get_name());
   auto msgbuf = msg_wrapper::package(hash, device_id, in, len);
@@ -244,9 +260,9 @@ int dev_write(int device_id, const void *in, size_t len) {
   return client._call(0, msgbuf.get(), msgbuf.size(), nullptr, nullptr);
 }
 int dev_read(int device_id, void *out, size_t len) {
-  int driver_id = get_driver_id(device_id);
-  auto iter = get_driver_map().end();
-  int res = search_driver(driver_id, &iter);
+  int driver_id = internal_query::get_driver_id(device_id);
+  internal_query::driver_map_iter iter;
+  int res = internal_query::search_driver(driver_id, &iter);
   if (res) return res;
   auto hash = gen_hash("read", iter->second->get_name());
   msg_ref refmsg(out, len);
@@ -257,9 +273,9 @@ int dev_read(int device_id, void *out, size_t len) {
 }
 
 int dev_ioctl(int device_id, int cmds, void *in_out, size_t in_len, size_t buffer_max_len) {
-  int driver_id = get_driver_id(device_id);
-  auto iter = get_driver_map().end();
-  int res = search_driver(driver_id, &iter);
+  int driver_id = internal_query::get_driver_id(device_id);
+  internal_query::driver_map_iter iter;
+  int res = internal_query::search_driver(driver_id, &iter);
   if (res) return res;
   auto hash = gen_hash("ioctl", iter->second->get_name());
   msg_ref refmsg(in_out, buffer_max_len);
